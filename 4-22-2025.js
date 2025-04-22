1549e1943faf0edbfc1b83a9b3c7b46ad2ed995f
Minindu Bimsara Abeywardena <minindubim@gmail.com>
2025-04-22 12:08:36 +0530

    Refactor hash function to convert input to integer array and return hashed values

--- 4-22-2025.js
+++ 4-22-2025.js
@@ -1,7 +1,21 @@
-function hash(str) {
-    let hash;
+function hash(input) {
+  let intArray = [];
+  let hashedArray = [];
 
+  // Convert the input to an integer array
+  const str = input.toString();
+  for (let i = 0; i < str.length; i++) {
+    intArray.push(Number(str[i]));
+  }
 
-    
-    return hash;
+  // Hash the array by modulating each element by 3
+  for (let i = 0; i < intArray.length; i++) {
+    hashedArray.push(intArray[i] % 3);
+  }
+
+  return hashedArray;
 }
+
+const input = 1234567890;
+const hashedArray = hash(input);
+console.log(hashedArray);
